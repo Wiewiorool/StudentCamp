d605cbadd3f5be09e5ea2b498fa7df25476504b5
Wiewior <wasisnkipatryk@gmail.com>
2025-03-03 22:16:11 +0100

    Add tests to addStudent method.

---
+++ src/test/java/pl/wiewior/studentcamp/CampOrganisationTest.java
@@ -0,0 +1,46 @@
+package pl.wiewior.studentcamp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CampOrganisationTest {
+    private CampOrganisation campOrganisation = new CampOrganisation(new ArrayList<>());
+
+    @Test
+    public void shouldNotAddStudentWhenFirstNameAndLastNameAreNull() {
+        //given
+        String newStudentFirstName = null;
+        String newStudentLastName = null;
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, newStudentLastName));
+        //then
+        Assertions.assertEquals("names cannot be null!", exception.getMessage());
+
+    }
+
+    @Test
+    public void shouldAddStudentWhenFirstNameAndLastNameAreNotNull() {
+        //given
+        String newStudentFirstName = "Andrew";
+        String newStudentLastName = "Adams";
+
+        //when
+        boolean exception = campOrganisation.addStudent(newStudentFirstName, newStudentLastName);
+        //then
+        Assertions.assertTrue(exception);
+
+    }
+
+    @Test
+    void transformName() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+}

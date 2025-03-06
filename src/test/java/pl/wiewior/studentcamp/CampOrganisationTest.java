92223fc078801c4a7fe9483e31b7450b5f5ff95e
Wiewior <wasisnkipatryk@gmail.com>
2025-03-06 18:58:05 +0100

    Add ParameterizedTests, clean code in tests.

--- src/test/java/pl/wiewior/studentcamp/CampOrganisationTest.java
+++ src/test/java/pl/wiewior/studentcamp/CampOrganisationTest.java
@@ -2,139 +2,86 @@ package pl.wiewior.studentcamp;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CampOrganisationTest {
     private CampOrganisation campOrganisation = new CampOrganisation(new ArrayList<>());
 
-    @Test
-    public void shouldNotAddStudentWhenFirstNameAndLastNameAreNull() {
-        //given
-        String newStudentFirstName = null;
-        String newStudentLastName = null;
+    public static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(null, null),
+                Arguments.of(null, "Gerard"),
+                Arguments.of("Roman", null)
+        );
+    }
 
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(null, "Bond", "null Bond"),
+                Arguments.of("James", null, "James null"),
+                Arguments.of(null, null, "null null"),
+                Arguments.of("James", "Bond", "James Bond")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void shouldNotAddStudentWhenFirstNameAndLastNameAreNull(String firstName, String lastName) {
+        //given
         //when
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, newStudentLastName));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(firstName, lastName));
         //then
         Assertions.assertEquals("names cannot be null!", exception.getMessage());
-
     }
 
     @Test
-    public void shouldNotAddStudentWhenFirstNameIsNull() {
-        //given
-        String newStudentFirstName = null;
-        String newStudentLastName = "Gerard";
-
-        //when
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, newStudentLastName));
-        //then
-        Assertions.assertEquals("names cannot be null!", exception.getMessage());
-
-    }
-
-    @Test
-    public void shouldNotAddStudentWhenLastNameIsNull() {
-        //given
-        String newStudentFirstName = "Roman";
-        String newStudentLastName = null;
-
-        //when
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> campOrganisation.addStudent(newStudentFirstName, newStudentLastName));
-        //then
-        Assertions.assertEquals("names cannot be null!", exception.getMessage());
-
-    }
-
-    @Test
-    public void shouldAddStudentWhenFirstNameAndLastNameAreNotNull() {
+    public void shouldAddStudent() {
         //given
         String newStudentFirstName = "Andrew";
         String newStudentLastName = "Adams";
 
         //when
-        boolean exception = campOrganisation.addStudent(newStudentFirstName, newStudentLastName);
+        boolean actual = campOrganisation.addStudent(newStudentFirstName, newStudentLastName);
         //then
-        assertTrue(exception);
-
+        assertTrue(actual);
     }
 
-    @Test
-    public void shouldReturnFirstAndLastNameOfStudent() {
+    @ParameterizedTest
+    @MethodSource("data")
+    public void shouldReturnNullAndLastNameOfStudent(String firstName, String lastName, String expected) {
         //given
-        String newStudentFirstName = "James";
-        String newStudentLastName = "Bond";
         //when
-        String transformName = campOrganisation.transformName(newStudentFirstName, newStudentLastName);
+        String transformName = campOrganisation.transformName(firstName, lastName);
         //then
-        Assertions.assertEquals("James Bond", transformName);
-
+        Assertions.assertEquals(expected, transformName);
     }
-
-    @Test
-    public void shouldReturnNullAndLastNameOfStudent() {
-        //given
-        String newStudentFirstName = null;
-        String newStudentLastName = "Bond";
-        //when
-        String transformName = campOrganisation.transformName(newStudentFirstName, newStudentLastName);
-        //then
-        Assertions.assertEquals("null Bond", transformName);
-
-    }
-
-    @Test
-    public void shouldReturnFirstNameAndNullOfStudent() {
-        //given
-        String newStudentFirstName = "James";
-        String newStudentLastName = null;
-        //when
-        String transformName = campOrganisation.transformName(newStudentFirstName, newStudentLastName);
-        //then
-        Assertions.assertEquals("James null", transformName);
-
-    }
-
-    @Test
-    public void shouldReturnNullAndNullOfStudent() {
-        //given
-        String newStudentFirstName = null;
-        String newStudentLastName = null;
-        //when
-        String transformName = campOrganisation.transformName(newStudentFirstName, newStudentLastName);
-        //then
-        Assertions.assertEquals("null null", transformName);
-
-    }
-
 
     @Test
     void shouldReturnWhenStudentExist() {
         //given
         String firstName = "Josef";
         String lastName = "Adams";
-        campOrganisation.addStudent(firstName,lastName);
+        campOrganisation.addStudent(firstName, lastName);
         //when
         String actual = campOrganisation.getStudent(firstName);
         //then
         Assertions.assertEquals("Josef Adams", actual);
-
     }
+
     @Test
     void shouldReturnWhenStudentNotExist() {
         //given
         String firstName = "Josef";
-        String lastName = "Adams";
         //when
         String actual = campOrganisation.getStudent(firstName);
         //then
         Assertions.assertEquals("", actual);
-
     }
-
-
 }

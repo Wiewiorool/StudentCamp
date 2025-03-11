cc7704894609efe8e21b5201e2a9cba1c96a1c3e
Wiewior <wasisnkipatryk@gmail.com>
2025-03-11 20:06:05 +0100

    Clean code in tests

--- src/test/java/pl/wiewior/taxes/InRelationshipChildLessTest.java
+++ src/test/java/pl/wiewior/taxes/InRelationshipChildLessTest.java
@@ -2,23 +2,33 @@ package pl.wiewior.taxes;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InRelationshipChildLessTest {
     private InRelationshipChildLess inRelationshipChildLess = new InRelationshipChildLess();
 
-    @Test
-    void calculateTax() {
-        //given
-        BigDecimal salary = BigDecimal.valueOf(10000);
-        BigDecimal expected = salary.multiply(BigDecimal.valueOf(0.25));
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(new BigDecimal(10000), new BigDecimal(0.25)),
+                Arguments.of(new BigDecimal(0), new BigDecimal(0.25)),
+                Arguments.of(new BigDecimal(-10000), new BigDecimal(0.25))
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("data")
+    void calculateTax(BigDecimal salary, BigDecimal tax) {
+        //given
         //when
         BigDecimal actual = inRelationshipChildLess.calculateTax(salary);
         //then
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(salary.multiply(tax).setScale(2), actual);
     }
 }
